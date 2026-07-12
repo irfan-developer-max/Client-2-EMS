7cdbb1b86cc710eea869f726f3efc44ed3760e78
irfan-developer-max <iamirfan6767@gmail.com>
2026-07-12 21:42:32 +0530

    Employee management component intiliationtion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 import Login from './pages/Login';
 import AdminDashboard from './pages/Admindashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard';
 function App() {
   return (
   <>
@@ -9,6 +10,7 @@ function App() {
     <Route path="/" element={<Navigate  to="admin-dashboard"/>} />
     <Route path="/login" element={<Login />} />
     <Route path="/admin-dashboard" element={<AdminDashboard />} />
+    <Route path="/employee-dashboard" element={<EmployeeDashboard/>}/>
   </Routes>
   </BrowserRouter>
   </>
