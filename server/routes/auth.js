eef9176574e4715da113ba9801515f6ed9e7529a
irfan-developer-max <iamirfan6767@gmail.com>
2026-08-02 11:19:10 +0530

    Authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,7 @@
 import express from "express";
-import {login} from "../controllers/authController.js";
+import {login,verify} from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 const router=express.Router()
 router.post('/login',login)
-router.get('/test',(req,res)=>{
-    console.log("auth test")
-    res.send("auh test")
-})
+router.get('/verify',authMiddleware,verify)
 export default router;
